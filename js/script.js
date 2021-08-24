59e2e966a33c86c65cdd3288807d5ee1e83bc30f
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-08-24 00:47:13 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const insertLi = lis =>{
+    const ul = document.querySelector('.container');
+    ul.innerHTML = lis;
+}
+
+fetch(`https://covid19-brazil-api.now.sh/api/report/v1`)
+.then(response => response.json())
+.then(json => json.data.reduce( (acc, uf) =>{
+
+    return acc += `<li>
+    <h2>${uf.state}</h2>
+    <p>casos: ${uf.cases}</p>
+    <p>óbitos: ${uf.deaths}</p>
+    <p>suspeitos: ${uf.suspects}</p>
+    <p>data e hora: ${uf.datetime}</p>
+    </li>`;
+}, ''))
+.then(insertLi)
